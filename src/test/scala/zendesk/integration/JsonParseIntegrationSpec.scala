e901759f4841a3844aa37e17b0d753ed15af1f12
Daniel Choi <daniel.choi@rea-group.com>
2019-12-28 19:47:17 +1100

    Add integration tests for `organizations.json` file

--- src/test/scala/zendesk/integration/JsonParseIntegrationSpec.scala
+++ src/test/scala/zendesk/integration/JsonParseIntegrationSpec.scala
@@ -4,22 +4,34 @@ import java.nio.file.{Files, Path}
 
 import io.circe
 import org.specs2.mutable.Specification
-import zendesk.model.User
+import zendesk.model.{Organization, User}
 
 import scala.io.Source
 import io.circe.parser.decode
 
 class JsonParseIntegrationSpec extends Specification {
 
-  "JSON string for a users, data/users.json file" >> {
+  "JSON string for users, data/users.json file" >> {
     val fileContents = Source.fromFile("./data/users.json").getLines.mkString
     val users: Either[circe.Error, List[User]] = decode[List[User]](fileContents)
 
-    //❯ cat data/users.json | jq '. | length'
-    //75
+    // ❯ cat data/users.json | jq '. | length'
+    // 75
     "JSON file should be parsed without failure" >> {
       users must beRight()
-      users.map(_.size) must be(Right(75))
+      users.map(_.size) must beRight(75)
+    }
+  }
+
+  "JSON string for organizations, data/organizations.json file" >> {
+    val fileContents = Source.fromFile("./data/organizations.json").getLines.mkString
+    val users: Either[circe.Error, List[Organization]] = decode[List[Organization]](fileContents)
+
+    // ❯ cat data/organizations.json | jq '. | length'
+    // 25
+    "JSON file should be parsed without failure" >> {
+      users must beRight()
+      users.map(_.size) must beRight(25)
     }
   }
 }
