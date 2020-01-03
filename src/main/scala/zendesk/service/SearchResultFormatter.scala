dc2b3f1a52385d0a2c8b377e224f8802afa4afd3
Daniel Choi <daniel.choi@rea-group.com>
2020-01-03 13:01:32 +1100

    Add logic to format `Relations`

--- src/main/scala/zendesk/service/SearchResultFormatter.scala
+++ src/main/scala/zendesk/service/SearchResultFormatter.scala
@@ -1,6 +1,8 @@
 package zendesk.service
 
 import io.circe.syntax._
+import zendesk.model.SearchResult.{AssigneeId, ForeignKey, OrganizationIdAtTicket, OrganizationIdAtUser, SubmitterId}
+import zendesk.model.Searchable.{Tickets, Users}
 import zendesk.model.{Organization, QueryParams, SearchResult, Searchable, Ticket, User}
 
 case class SearchResultFormatter() {
@@ -9,12 +11,16 @@ case class SearchResultFormatter() {
       title(sr.queryParams)
     }.toVector
 
-    val reportContents = searchResults.map { sr: SearchResult =>
-      s"""${sr.primaryObject.asFullDataString}
-         |---------------------------------------------------""".stripMargin
-    }
+    val reportContentsForPrimaryObject =
+      searchResults.map { sr: SearchResult =>
+        s"""## Primary Object
+           |${sr.primaryObject.asFullDataString}
+           |
+           |${formatRelations(sr.queryParams, sr.relations)}
+           |---------------------------------------------------""".stripMargin
+      }
 
-    (reportTitle ++ reportContents).mkString("")
+    (reportTitle ++ reportContentsForPrimaryObject).mkString("")
   }
 
   private def title(queryParams: QueryParams): String = {
@@ -26,5 +32,30 @@ case class SearchResultFormatter() {
        |   - Value: ${queryParams.searchValue.rawValue}
        |---------------------------------------------------
        |""".stripMargin
+  }
+
+  private def formatRelations(queryParams: QueryParams, relations: Map[ForeignKey, Vector[Searchable]]): String = {
+    val header = "## Relations"
+
+    val descriptions = relations.toVector.map { relation: (ForeignKey, Vector[Searchable]) =>
+      val body = relation._2.map(_.asSimpleDataString).mkString("")
+
+      s"""${subHeader(queryParams.searchKey, relation._1)}
+         |$body
+         |
+         |""".stripMargin
+    }.mkString("")
+
+    s"""$header
+       |$descriptions""".stripMargin
+  }
+
+  private def subHeader(value: (Searchable.Keys, ForeignKey)): String = value match {
+    case (Users, SubmitterId) => s"### Tickets as Submitter"
+    case (Tickets, SubmitterId) => s"### Users as Submitter"
+    case (Users, AssigneeId) => s"### Tickets as Assignee"
+    case (Tickets, AssigneeId) => s"### Users as Assignee"
+    case (_, OrganizationIdAtTicket) | (_, OrganizationIdAtUser) => "### Organizations"
+    case _ => ""
   }
 }
--- src/test/scala/zendesk/service/SearchResultFormatterTest.scala
+++ src/test/scala/zendesk/service/SearchResultFormatterTest.scala
@@ -4,6 +4,7 @@ import java.util.UUID
 
 import org.specs2.mutable.Specification
 import zendesk.helper.TestDataFactory
+import zendesk.model.SearchResult.ForeignKey
 import zendesk.model.value.{Id, TicketId}
 import zendesk.model.{QueryParams, SearchResult, Searchable}
 class SearchResultFormatterTest extends Specification {
@@ -26,36 +27,31 @@ class SearchResultFormatterTest extends Specification {
              |""".stripMargin
 
         val expectedSearchResultForPrimaryObject =
-          s"""
-             |  _id : 1
-             |  url : http://initech.zendesk.com/api/v2/users/1.json
-             |  external_id : 74341f74-9c79-49d5-9611-87ef9b6eb75f
-             |  name : Francisca Rasmussen
-             |  alias : Miss Coffey
-             |  created_at : 2016-05-21T11:10:28 -10:00
-             |  active : true
-             |  verified : true
-             |  shared : false
-             |  locale : en-AU
-             |  timezone : Sri Lanka
-             |  last_login_at : 2016-05-21T11:10:28 -10:00
-             |  email : coffeyrasmussen@flotonic.com
-             |  phone : 8335-422-718
-             |  signature : Don't Worry Be Happy!
-             |  organization_id : 119
-             |  tags : [
-             |    Springville
-             |    Sutton
-             |    Diaperville
-             |  ]
-             |  suspended : true
-             |  role : admin
+          s"""## Primary Object
+             |${user.asFullDataString}
+             |
+             |## Relations
+             |### Tickets as Submitter
+             |${ticket.asSimpleDataString}
+             |
+             |### Tickets as Assignee
+             |${ticket.asSimpleDataString}
+             |
+             |### Organizations
+             |${organization.asSimpleDataString}
+             |
+             |
              |---------------------------------------------------""".stripMargin
 
         val queryParams = {
           QueryParams(Searchable.Users, "Id", Id(1))
         }
-        val searchResult = SearchResult(queryParams, user, Map.empty)
+        val relations = Map[ForeignKey, Vector[Searchable]](
+          SearchResult.SubmitterId -> Vector(ticket),
+          SearchResult.AssigneeId -> Vector(ticket),
+          SearchResult.OrganizationIdAtUser -> Vector(organization)
+        )
+        val searchResult = SearchResult(queryParams, user, relations)
         val expectedResultString = searchResultTitle + expectedSearchResultForPrimaryObject
 
         val result = formatter.format(Vector(searchResult))
@@ -74,32 +70,31 @@ class SearchResultFormatterTest extends Specification {
              |""".stripMargin
 
         val expectedSearchResultForPrimaryObject =
-          s"""
-             |  _id : 436bf9b0-1147-4c0a-8439-6f79833bff5b
-             |  url : http://initech.zendesk.com/api/v2/tickets/436bf9b0-1147-4c0a-8439-6f79833bff5b.json
-             |  external_id : 9210cdc9-4bee-485f-a078-35396cd74063
-             |  created_at : 2016-04-28T11:19:34 -10:00
-             |  type : incident
-             |  subject : A Catastrophe in Korea (North)
-             |  description : Nostrud ad sit velit cupidatat laboris ipsum nisi amet laboris
-             |  priority : high
-             |  status : pending
-             |  submitter_id : 38
-             |  assignee_id : 24
-             |  organization_id : 116
-             |  tags : [
-             |    Ohio
-             |    Pennsylvania
-             |  ]
-             |  has_incidents : false
-             |  due_at : 2016-07-31T02:37:50 -10:00
-             |  via : web
+          s"""## Primary Object
+             |${ticket.asFullDataString}
+             |
+             |## Relations
+             |### Users as Submitter
+             |${user.asSimpleDataString}
+             |
+             |### Users as Assignee
+             |${user.asSimpleDataString}
+             |
+             |### Organizations
+             |${organization.asSimpleDataString}
+             |
+             |
              |---------------------------------------------------""".stripMargin
 
         val queryParams = {
           QueryParams(Searchable.Tickets, "Id", TicketId(UUID.fromString("436bf9b0-1147-4c0a-8439-6f79833bff5b")))
         }
-        val searchResult = SearchResult(queryParams, ticket, Map.empty)
+        val relations = Map[ForeignKey, Vector[Searchable]](
+          SearchResult.SubmitterId -> Vector(user),
+          SearchResult.AssigneeId -> Vector(user),
+          SearchResult.OrganizationIdAtTicket -> Vector(organization)
+        )
+        val searchResult = SearchResult(queryParams, ticket, relations)
         val expectedResultString = searchResultTitle + expectedSearchResultForPrimaryObject
 
         val result = formatter.format(Vector(searchResult))
@@ -118,32 +113,28 @@ class SearchResultFormatterTest extends Specification {
              |""".stripMargin
 
         val expectedSearchResultForPrimaryObject =
-          s"""
-             |  _id : 101
-             |  url : http://initech.zendesk.com/api/v2/organizations/101.json
-             |  external_id : 9270ed79-35eb-4a38-a46f-35725197ea8d
-             |  name : Enthaze
-             |  domain_names : [
-             |    kage.com
-             |    ecratic.com
-             |    endipin.com
-             |    zentix.com
-             |  ]
-             |  created_at : 2016-05-21T11:10:28 -10:00
-             |  details : MegaCorp
-             |  shared_tickets : false
-             |  tags : [
-             |    Fulton
-             |    West
-             |    Rodriguez
-             |    Farley
-             |  ]
+          s"""## Primary Object
+             |${organization.asFullDataString}
+             |
+             |## Relations
+             |### Organizations
+             |${user.asSimpleDataString}
+             |
+             |### Organizations
+             |${ticket.asSimpleDataString}
+             |
+             |
              |---------------------------------------------------""".stripMargin
 
         val queryParams = {
           QueryParams(Searchable.Organizations, "Id", Id(101))
         }
-        val searchResult = SearchResult(queryParams, organization, Map.empty)
+        val relations = Map[ForeignKey, Vector[Searchable]](
+          SearchResult.OrganizationIdAtUser -> Vector(user),
+          SearchResult.OrganizationIdAtTicket -> Vector(ticket)
+        )
+        val searchResult = SearchResult(queryParams, organization, relations)
+
         val expectedResultString = searchResultTitle + expectedSearchResultForPrimaryObject
 
         val result = formatter.format(Vector(searchResult))
